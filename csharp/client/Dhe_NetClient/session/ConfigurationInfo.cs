e58fffa1e3fb46e3d1bc821d5c25277d424d2645
Corey Kosak <kosak@users.noreply.github.com>
2025-10-04 14:42:23 -0400

    feat(csharp/client): DH-20522: Add SAML support to CorePlus client (#3)

---
+++ csharp/client/Dhe_NetClient/session/ConfigurationInfo.cs
@@ -0,0 +1,69 @@
+﻿using Grpc.Core;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Web;
+
+namespace Deephaven.Dhe_NetClient;
+
+public sealed class ConfigurationInfo {
+  [JsonPropertyName("auth_host")]
+  public required string[] AuthHost { get; init; }
+  [JsonPropertyName("auth_port")]
+  public required UInt16 AuthPort { get; init; }
+  [JsonPropertyName("controller_host")]
+  public required string ControllerHost { get; init; }
+  [JsonPropertyName("controller_port")]
+  public required UInt16 ControllerPort { get; init; }
+  [JsonPropertyName("truststore_url")]
+  public string? TruststoreUrl { get; init; }
+  [JsonPropertyName("override_authorities")]
+  public bool OverrideAuthorities { get; init; }
+  [JsonPropertyName("auth_authority")]
+  public string? AuthAuthority { get; init; }
+  [JsonPropertyName("controller_authority")]
+  public string? ControllerAuthority { get; init; }
+  [JsonPropertyName("saml_sso_uri")]
+  public string? SamlSsoUri { get; init; }
+
+  public static ConfigurationInfo OfJson(string input) {
+    var result = JsonSerializer.Deserialize<ConfigurationInfo>(input);
+    return result ?? throw new Exception("JsonSerializer returned null");
+  }
+
+  public static ConfigurationInfo OfUrl(string url, bool validateCertificate) {
+    var handler = new HttpClientHandler();
+    if (!validateCertificate) {
+      handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+      handler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
+    }
+    using var hc = new HttpClient(handler);
+    var json = hc.GetStringAsync(url).Result;
+
+    return OfJson(json);
+  }
+
+  public bool TryMakeSamlAuthUrl(string nonce,
+    [MaybeNullWhen(false)] out string url,
+    [MaybeNullWhen(true)] out string errorText) {
+    url = null;
+    errorText = null;
+
+    if (SamlSsoUri == null) {
+      errorText = "Server not configured for SAML";
+      return false;
+    }
+
+    try {
+      var uriBuilder = new UriBuilder(SamlSsoUri);
+      var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+      query["key"] = nonce;
+      uriBuilder.Query = query.ToString();
+      url = uriBuilder.ToString();
+      return true;
+    } catch (Exception e) {
+      errorText = $"Malformed SAML URI from configuration: {e.Message}";
+      return false;
+    }
+  }
+}
